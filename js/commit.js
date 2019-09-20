5cd12d8fb6ef5ae55ff503db566b3c024d45d91a
Natehubbit <natehubbit1001@gmail.com>
2019-09-20 14:52:26 +0000

    CloudUpload Commit

---
+++ js/commit.js
@@ -0,0 +1,50 @@
+$(document).ready(()=>{
+
+    var cloudUrl = 'https://api.cloudinary.com/v1_1/dednjremi/upload'
+    var uploadPreset = 'rrqprh2f'
+    var file=null
+
+    
+
+    $('#fabric_image').on('change',e=>{
+        file = e.target.files[0]
+    })
+
+    $('#data-info').click(function(e){
+        e.preventDefault();
+
+        var formData = new FormData()
+        formData.append('file',file)
+        console.log(file)
+        formData.append('upload_preset',uploadPreset)
+
+        axios({
+            url:cloudUrl,
+            method: 'POST',
+            headers:{
+                'Content-Type':'application/x-www-form-urlencoded',
+            },
+            data:formData
+        }).then(res=>{
+            var imageUrl = res.data.secure_url
+            axios.post('https://rose-pay.herokuapp.com/finder/', {
+                'resource': [{
+                    'name': 'image_finder',
+                    'field': [{
+                        'fabric_image': imageUrl,
+                        'description': $("#description").val(),
+                        'phone_number': $("#phone_number").val()
+                    }]
+                }]
+            }, {
+                headers: {
+                    'Devless-token': '516d707a35bf9714d30b3c8273c5d66c'
+                }
+            })
+        })
+        .catch(err=>console.log(err))
+      });
+
+    
+
+})
